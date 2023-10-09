8cca244e5b9366ff3dcdfc128715f605b25f1efb
AdEmanuel <68473645+AdEmanuel@users.noreply.github.com>
2023-10-09 01:29:29 +0000

    Updates for pylint

--- Python_Essentials_for_MLOps/Project 01/movie_recommendation.py
+++ Python_Essentials_for_MLOps/Project 01/movie_recommendation.py
@@ -1,3 +1,10 @@
+"""
+Este script realiza recomendações de filmes com base em um título fornecido
+pelo usuário. Ele baixa um conjunto de dados de filmes, carrega os dados
+necessários em DataFrames do Pandas, e utiliza técnicas de processamento de texto
+e similaridade de cosseno para encontrar filmes semelhantes ao título fornecido.
+
+"""
 import os
 import re
 import logging
@@ -6,9 +13,7 @@ import argparse
 import requests
 import numpy as np
 import pandas as pd
-import ipywidgets as widgets
 from tqdm import tqdm
-from IPython.display import display
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -16,183 +21,183 @@ from sklearn.metrics.pairwise import cosine_similarity
 logging.basicConfig(level=logging.INFO)
 
 def download_dataset() -> None:
-  """
-  Realiza o download e extração de um conjunto de dados.
+    """
+    Realiza o download e extração de um conjunto de dados.
 
-  Returns:
+    Returns:
       None
-  """
-  # URL do arquivo zip
-  url = "http://files.grouplens.org/datasets/movielens/ml-25m.zip"
+    """
+    # URL do arquivo zip
+    url="http://files.grouplens.org/datasets/movielens/ml-25m.zip"
 
-  # Nome do arquivo zip a ser baixado
-  zip_filename = "ml-25m.zip"
-  
-  # Nome da pasta a ser extraída
-  extracted_folder = "ml-25m"
+    # Nome do arquivo zip a ser baixado
+    zip_filename = "ml-25m.zip"
 
-  if os.path.exists(extracted_folder):
-    logging.info(" Os arquivos necessários já existem")
-    return
+    # Nome da pasta a ser extraída
+    extracted_folder = "ml-25m"
 
-  try:
+    if os.path.exists(extracted_folder):
+        logging.info("Os arquivos necessários já existem")
+        return
 
-    logging.info("Iniciando Download...")
+    try:
 
-    with requests.Session() as session:
-      response = session.get(url, stream=True)
-      response.raise_for_status()
+        logging.info("Iniciando Download...")
 
-      with open(zip_filename, 'wb') as zip_file, tqdm(
-        desc=f"Progresso do Download: ",
-        total=int(response.headers.get('content-length', 0)),
-        unit='B',
-        unit_scale=True,
-        unit_divisor=1024,
-      ) as bar:
-        for data in response.iter_content(chunk_size=1024):
-          zip_file.write(data)
-          bar.update(len(data))
+        with requests.Session() as session:
+            response = session.get(url, stream=True)
+            response.raise_for_status()
 
-    logging.info(f"Arquivo {zip_filename} baixado com sucesso.")
-    logging.info(f"Extraindo arquivos ...")
+        with open(zip_filename, 'wb') as zip_file, tqdm(
+            desc="Progresso do Download: ",
+            total=int(response.headers.get('content-length', 0)),
+            unit='B',
+            unit_scale=True,
+            unit_divisor=1024,
+        ) as pbar:
+            for data in response.iter_content(chunk_size=1024):
+                zip_file.write(data)
+                pbar.update(len(data))
 
-    with zipfile.ZipFile(zip_filename, 'r') as zip_file:
-      file_list = zip_file.namelist()
-      with tqdm(
-        desc=f"Progresso da Extração: ",
-        total=len(file_list),
-        unit="files",) as pbar:
-        for file_name in file_list:
-          zip_file.extract(file_name,".")
-          pbar.update(1)
+        logging.info("Arquivo %s baixado com sucesso.", zip_filename)
+        logging.info("Extraindo arquivos ...")
 
-    # Exclui o arquivo .zip após a extração correta
-    os.remove(zip_filename)
-    logging.info(f"Arquivo {zip_filename} removido com sucesso.")
+        with zipfile.ZipFile(zip_filename, 'r') as zip_file:
+            file_list = zip_file.namelist()
+            with tqdm(
+                desc="Progresso da Extração: ",
+                total=len(file_list),
+                unit="files",) as pbar:
+                for file_name in file_list:
+                    zip_file.extract(file_name,".")
+                    pbar.update(1)
 
-  except requests.exceptions.ConnectionError:
-      logging.error(f"Erro de conexão ao baixar o arquivo")
-  except requests.exceptions.RequestException as request_error:
-      logging.error(f"Erro de requisição ao baixar o arquivo: {request_error}")
-  except zipfile.BadZipFile as zip_error:
-      logging.error(f"Erro ao extrair o arquivo {zip_error}")
-  except Exception as e:
-      logging.error(f"Ocorreu um erro inesperado: {str(e)}")
+        # Exclui o arquivo .zip após a extração correta
+        os.remove(zip_filename)
+        logging.info("Arquivo %s removido com sucesso.", zip_filename)
+
+    except requests.exceptions.ConnectionError:
+        logging.error("Erro de conexão ao baixar o arquivo")
+    except requests.exceptions.RequestException as request_error:
+        logging.error("Erro de requisição ao baixar o arquivo: %s", request_error)
+    except zipfile.BadZipFile as zip_error:
+        logging.error("Erro ao extrair o arquivo %s", zip_error)
+    except ValueError as error:
+        logging.error("Ocorreu um erro inesperado: %s", str(error))
 
 def load_csv(filepath: str) -> pd.DataFrame:
-  """
-  Carrega um arquivo CSV em um DataFrame.
+    """
+    Carrega um arquivo CSV em um DataFrame.
 
-  Args:
-      filepath (str): Caminho para o arquivo CSV a ser carregado.
+    Args:
+        filepath (str): Caminho para o arquivo CSV a ser carregado.
 
-  Returns:
-      pd.DataFrame: Um DataFrame contendo os dados do arquivo CSV.
-  """
-  try:
-    # Carrega o arquivo CSV em um DataFrame
-    df = pd.read_csv(filepath)
-    return df
-  except Exception as e:
-    print(f"Ocorreu um erro ao carregar o arquivo CSV: {str(e)}")
-    return None
+    Returns:
+        pd.DataFrame: Um DataFrame contendo os dados do arquivo CSV.
+    """
+    try:
+        # Carrega o arquivo CSV em um DataFrame
+        data_frame = pd.read_csv(filepath)
+        return data_frame
+    except Exception as error:
+        print("Ocorreu um erro ao carregar o arquivo CSV: %s", str(error))
+        return None
 
-def clean_movie_title(movie_title: str) -> str:
-  """
-  Remove os caracteres especiais presentes nos nome do filme
+def clean_movie_title(movie_title_input: str) -> str:
+    """
+    Remove os caracteres especiais presentes nos nome do filme
 
-  Args:
-    movie_title (str): Título do filme que será "limpado"
+    Args:
+      movie_title_input (str): Título do filme que será "limpado"
 
-  Return:
-    str: Título do filme sem caracters especiais
-  """
+    Return:
+      str: Título do filme sem caracters especiais
+    """
 
-  cleaned_movie_title = re.sub("[^a-zA-Z0-9 ]", "", movie_title)
-  return cleaned_movie_title
+    cleaned_movie_title = re.sub("[^a-zA-Z0-9 ]", "", movie_title_input)
+    return cleaned_movie_title
 
 def search_movie_by_title_similarity(query_title: str) -> pd.DataFrame:
-  """
-  Realiza uma pesquisa de similaridade com base no título fornecido
+    """
+    Realiza uma pesquisa de similaridade com base no título fornecido
 
-  Args:
-    query_title (str): Título do filme a qual se deseja encontrar similiares
+    Args:
+      query_title (str): Título do filme a qual se deseja encontrar similiares
 
-  Returns:
-    pd.DataFrame: Dataframe que contém os 5 títulos mais similires ao consultado
+    Returns:
+      pd.DataFrame: Dataframe que contém os 5 títulos mais similires ao consultado
 
-  """
-  cleaned_query_title = clean_movie_title(query_title)
-  query_vector = tfidf_vectorizer.transform([cleaned_query_title])
-  cosine_similarities = cosine_similarity(query_vector, tfidf_matrix).flatten()
-  top_movie_indices = np.argpartition(cosine_similarities, -5)[-5:]
-  top_movie_results = movies_df.iloc[top_movie_indices].iloc[::-1]
-  
-  return top_movie_results
+    """
+    cleaned_query_title = clean_movie_title(query_title)
+    query_vector = tfidf_vectorizer.transform([cleaned_query_title])
+    cosine_similarities = cosine_similarity(query_vector, tfidf_matrix).flatten()
+    top_movie_indices = np.argpartition(cosine_similarities, -5)[-5:]
+    top_movie_results = movies_df.iloc[top_movie_indices].iloc[::-1]
 
-def find_similar_movies(movie_id: int) -> pd.DataFrame:
-  """
-  Encontra filmes semelhantes com base em um filme de referência.
+    return top_movie_results
 
-  Args:
-    movie_id (int): ID do filme que se deseja encontrar filmes semelhantes
+def find_similar_movies(reference_movie_id: int) -> pd.DataFrame:
+    """
+    Encontra filmes semelhantes com base em um filme de referência.
 
-  Returns:
-    pd.DataFrame: DataFrame contendo os filmes mais semelhantes ao filme de referência
-  """
+    Args:
+      movie_id (int): ID do filme que se deseja encontrar filmes semelhantes
 
-  similar_users = ratings_df[(ratings_df["movieId"] == movie_id) & (ratings_df["rating"] > 4)]["userId"].unique()
-  similar_user_recs = ratings_df[(ratings_df["userId"].isin(similar_users)) & (ratings_df["rating"] > 4)]["movieId"]
-  similar_user_recs = similar_user_recs.value_counts() / len(similar_users)
+    Returns:
+      pd.DataFrame: DataFrame contendo os filmes mais semelhantes ao filme de referência
+    """
 
-  similar_user_recs = similar_user_recs[similar_user_recs > .10]
-  all_users = ratings_df[(ratings_df["movieId"].isin(similar_user_recs.index)) & (ratings_df["rating"] > 4)]
-  all_user_recs = all_users["movieId"].value_counts() / len(all_users["userId"].unique())
-  rec_percentages = pd.concat([similar_user_recs, all_user_recs], axis=1)
-  rec_percentages.columns = ["similar", "all"]
+    similar_users = ratings_df[(ratings_df["movieId"] == reference_movie_id) & (ratings_df["rating"] > 4)]["userId"].unique()
+    similar_user_recs = ratings_df[(ratings_df["userId"].isin(similar_users)) & (ratings_df["rating"] > 4)]["movieId"]
+    similar_user_recs = similar_user_recs.value_counts() / len(similar_users)
 
-  rec_percentages["score"] = rec_percentages["similar"] / rec_percentages["all"]
-  rec_percentages = rec_percentages.sort_values("score", ascending=False)
+    similar_user_recs = similar_user_recs[similar_user_recs > .10]
+    all_users = ratings_df[(ratings_df["movieId"].isin(similar_user_recs.index)) & (ratings_df["rating"] > 4)]
+    all_user_recs = all_users["movieId"].value_counts() / len(all_users["userId"].unique())
+    rec_percentages = pd.concat([similar_user_recs, all_user_recs], axis=1)
+    rec_percentages.columns = ["similar", "all"]
 
-  return rec_percentages.head(10).merge(movies_df, left_index=True, right_on="movieId")[["score", "title", "genres"]]
+    rec_percentages["score"] = rec_percentages["similar"] / rec_percentages["all"]
+    rec_percentages = rec_percentages.sort_values("score", ascending=False)
+
+    return rec_percentages.head(10).merge(movies_df, left_index=True, right_on="movieId")[["score", "title", "genres"]]
 
 if __name__ == '__main__':
 
-  #Tratamento para receber o título do filme através da linha de comando
-  parser = argparse.ArgumentParser(description='Recomendador de Filmes')
-  parser.add_argument('-movie_title', type=str, help='Título do filme para pesquisa de similaridade')
-  args = parser.parse_args()
-  movie_title = args.movie_title
+    #Tratamento para receber o título do filme através da linha de comando
+    parser = argparse.ArgumentParser(description='Recomendador de Filmes')
+    parser.add_argument('-movie_title', type=str, help='Título do filme para pesquisa de similaridade')
+    args = parser.parse_args()
+    movie_title = args.movie_title
 
-  # Download dos datasets
-  download_dataset()
+    # Download dos datasets
+    download_dataset()
 
-  # Carrega os datasets necessários
-  movies_df = load_csv("./ml-25m/movies.csv")
-  ratings_df = load_csv("./ml-25m/ratings.csv")
+    # Carrega os datasets necessários
+    movies_df = load_csv("./ml-25m/movies.csv")
+    ratings_df = load_csv("./ml-25m/ratings.csv")
 
-  # Adicionando ao dataset nova coluna com títulos de filmes sem caracteres especiais
-  movies_df["clean_title"] = movies_df["title"].apply(clean_movie_title)
+    # Adicionando ao dataset nova coluna com títulos de filmes sem caracteres especiais
+    movies_df["clean_title"] = movies_df["title"].apply(clean_movie_title)
 
-  # Cria uma instância do TfidfVectorizer para calcular o TF-IDF
-  tfidf_vectorizer = TfidfVectorizer(ngram_range=(1, 2))
-  # Calcula o TF-IDF para os títulos de filmes limpos
-  tfidf_matrix = tfidf_vectorizer.fit_transform(movies_df["clean_title"])
+    # Cria uma instância do TfidfVectorizer para calcular o TF-IDF
+    tfidf_vectorizer = TfidfVectorizer(ngram_range=(1, 2))
+    # Calcula o TF-IDF para os títulos de filmes limpos
+    tfidf_matrix = tfidf_vectorizer.fit_transform(movies_df["clean_title"])
 
-  # Use a função de pesquisa para encontrar o filme correspondente
-  results = search_movie_by_title_similarity(movie_title)
+    # Use a função de pesquisa para encontrar o filme correspondente
+    results = search_movie_by_title_similarity(movie_title)
 
-  if not results.empty:
-    # Obtém o ID do filme correspondente
-    movie_id = results.iloc[0]["movieId"]
-    
-    # Função find_similar_movies encontra os 10 filmes mais similares
-    similar_movies = find_similar_movies(movie_id)
-    
-    # Exibe os resultados da busca
-    if similar_movies.empty:
-      print(f"Não há sugestões de similaridade para o filme '{movie_title}' ")
-    else: 
-      print(f"Os 10 filmes mais similares a {movie_title} são:")
-      print(similar_movies)
+    if not results.empty:
+        # Obtém o ID do filme correspondente
+        movie_id = results.iloc[0]["movieId"]
+
+        # Função find_similar_movies encontra os 10 filmes mais similares
+        similar_movies = find_similar_movies(movie_id)
+
+        # Exibe os resultados da busca
+        if similar_movies.empty:
+            print("Não há sugestões de similaridade para o filme '%s' ", movie_title)
+        else:
+            print("Os 10 filmes mais similares a %s são: ", movie_title)
+            print(similar_movies)
