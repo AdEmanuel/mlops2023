e604cddece4db6d5a1bfafcb8f27f9539b882620
AdEmanuel <68473645+AdEmanuel@users.noreply.github.com>
2023-10-10 01:25:32 +0000

    Update Fisinh

--- Python_Essentials_for_MLOps/Project 01/test_movie_recommendation.py
+++ Python_Essentials_for_MLOps/Project 01/test_movie_recommendation.py
@@ -1,13 +1,15 @@
-#import pytest
-import pandas as pd
-from movie_recommendation import clean_movie_title, search_movie_by_title_similarity, find_similar_movies
+"""
+Teste unitário para a função do sistema recomendador de filmes.
+"""
+
+from movie_recommendation import clean_movie_title
 
 def test_clean_movie_title():
-        
-        """
-        Testa a função clean_movie_title para garantir que ela remove os caracteres especiais das strings passadas
-        """
+    """
+    Testa a função clean_movie_title para garantir que ela remove os
+    caracteres especiais das strings passadas
+    """
 
-        assert clean_movie_title('Jumanji (1995)') == 'Jumanji 1995'
-        assert clean_movie_title('F.U.B.A.R (2019)') == 'FUBAR 2019'
-        assert clean_movie_title('Gumby: The Movie (1995)') == 'Gumby The Movie 1995'
+    assert clean_movie_title('Jumanji (1995)') == 'Jumanji 1995'
+    assert clean_movie_title('F.U.B.A.R (2019)') == 'FUBAR 2019'
+    assert clean_movie_title('Gumby: The Movie (1995)') == 'Gumby The Movie 1995'
